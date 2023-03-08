d1a3a2b4cb04c16aa80c1a4f11e0fe9ea237e9ac
suchan-redhat <suchan@redhat.com>
2023-03-08 16:12:24 +0800

    Update SpringbootHelloworldSeleniumApplication.java

--- src/main/java/com/redhat/com/springboothelloworldselenium/SpringbootHelloworldSeleniumApplication.java
+++ src/main/java/com/redhat/com/springboothelloworldselenium/SpringbootHelloworldSeleniumApplication.java
@@ -6,21 +6,13 @@ import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.remote.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootHelloworldSeleniumApplication {
 
         public static void main(String[] args) {
                 SpringApplication.run(SpringbootHelloworldSeleniumApplication.class, args);
-        }
-        
-        @Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-                    try {
+		try {
                         SpringApplication.run(SpringbootHelloworldSeleniumApplication.class, args);
                         ChromeOptions chromeOptions = new ChromeOptions();
                         chromeOptions.addArguments("--ignore-ssl-errors=yes");
@@ -28,11 +20,9 @@ public class SpringbootHelloworldSeleniumApplication {
                         WebDriver driver = new RemoteWebDriver(new URL("http://selenium-standalone-chrome-cicd-tools.apps.nonp-cluster.smartplay-np.lcsd.hksarg/wd/hub"), chromeOptions);
                         driver.get("http://springboot-demo-cicddemo-dev.apps.nonp-cluster.smartplay-np.lcsd.hksarg/greeting");
                         driver.quit();
-                    } catch (Exception e) {
-                        System.out.println("ERROR@@@@@");
-                    }
-
-		};
-	}
+		    } catch (Exception e) {
+			System.out.println("ERROR@@@@@");
+		    }
+        }
 
 }
