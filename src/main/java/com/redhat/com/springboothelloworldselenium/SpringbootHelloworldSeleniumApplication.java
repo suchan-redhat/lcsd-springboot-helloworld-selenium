96966e07ce28df08741a0565183ca4683c62348b
suchan-redhat <suchan@redhat.com>
2023-03-08 16:28:00 +0800

    Update SpringbootHelloworldSeleniumApplication.java

--- src/main/java/com/redhat/com/springboothelloworldselenium/SpringbootHelloworldSeleniumApplication.java
+++ src/main/java/com/redhat/com/springboothelloworldselenium/SpringbootHelloworldSeleniumApplication.java
@@ -14,11 +14,16 @@ public class SpringbootHelloworldSeleniumApplication {
                 SpringApplication.run(SpringbootHelloworldSeleniumApplication.class, args);
 		try {
                         SpringApplication.run(SpringbootHelloworldSeleniumApplication.class, args);
+			System.out.println("111111111");
                         ChromeOptions chromeOptions = new ChromeOptions();
+			System.out.println("22222222");
                         chromeOptions.addArguments("--ignore-ssl-errors=yes");
                         chromeOptions.addArguments("--ignore-certificate-errors=yes");
+			System.out.println("33333");
                         WebDriver driver = new RemoteWebDriver(new URL("http://selenium-standalone-chrome-cicd-tools.apps.nonp-cluster.smartplay-np.lcsd.hksarg/wd/hub"), chromeOptions);
-                        driver.get("http://springboot-demo-cicddemo-dev.apps.nonp-cluster.smartplay-np.lcsd.hksarg/greeting");
+                        System.out.println("444444");
+			driver.get("http://springboot-demo-cicddemo-dev.apps.nonp-cluster.smartplay-np.lcsd.hksarg/greeting");
+			System.out.println("55555");
                         driver.quit();
 		    } catch (Exception e) {
 			System.out.println("ERROR@@@@@");
