72bb10971f09dd3ff4c3801cbe1b06acfd21cd71
suchan-redhat <suchan@redhat.com>
2023-03-08 14:23:10 +0800

    Update SpringbootHelloworldSeleniumApplication.java

--- src/main/java/com/redhat/com/springboothelloworldselenium/SpringbootHelloworldSeleniumApplication.java
+++ src/main/java/com/redhat/com/springboothelloworldselenium/SpringbootHelloworldSeleniumApplication.java
@@ -1,19 +1,24 @@
-package com.redhat.com.springboothelloworldselenium;
-
+import java.net.URL;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.remote.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootHelloworldSeleniumApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootHelloworldSeleniumApplication.class, args);
-		ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.setCapability("browserVersion", "67");
-		chromeOptions.setCapability("platformName", "Windows XP");
-		WebDriver driver = new RemoteWebDriver(new URL("http://www.example.com"), chromeOptions);
-		driver.get("http://www.google.com");
-		driver.quit();
-	}
+        public static void main(String[] args) {
+            try {
+                SpringApplication.run(SpringbootHelloworldSeleniumApplication.class, args);
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--ignore-ssl-errors=yes");
+                chromeOptions.addArguments("--ignore-certificate-errors=yes");
+                WebDriver driver = new RemoteWebDriver(new URL("http://selenium-standalone-chrome.cicd-tools.svc.cluster.local/wd/hub"), chromeOptions);
+                driver.get("http://springboot-demo-cicddemo-dev.apps.nonp-cluster.smartplay-np.lcsd.hksarg/greeting");
+                driver.quit();
+            } catch (Exception e) {
+            }
+        }
 
 }
